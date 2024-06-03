99b9cf81f3d927ffbd603619542d328b5778de65
André Pereira <andre.jobs23@gmail.com>
2024-06-03 17:53:12 -0300

    Basic Structure and Stylings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
+import landscape1 from "./assets/pine-watt-unsplash.jpg"
+
+import "./assets/Tabs.css"
+
 function App() {
+  
+  const selectTabFunction = function() {
+    const selectTab = document.querySelectorAll(".tabs__info__menu li")
+
+    selectTab.map((tab) => {
+      tab.addEventListener('click', function() {
+        alert("test")
+      })
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Tabs</h1>
+
+      <div className='tabs__container'>
+          <div className='tabs__img'>
+            <img src={landscape1} width={200} height={200} alt="Landscape1"/>
+          </div>
+
+          <div className='tabs__info'>
+            <nav className="tabs__info__menu">
+              <ul>
+                <li className="selected">History</li>
+                <li>Vision</li>
+                <li>Goals</li>
+              </ul>
+            </nav>
+
+            <div className="tabs__info__description">
+              <h3>History</h3>
+              <p>
+                Text - Lorem Ipsum
+              </p>
+            </div>
+
+          </div>
+      </div>
     </div>
   );
 }
