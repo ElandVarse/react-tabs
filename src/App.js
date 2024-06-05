2d29169747c80d6d42656d0480ddff7c2ac982ef
André Pereira <andre.jobs23@gmail.com>
2024-06-05 12:01:45 -0300

    Scripts: Dynamic data change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
 
-import React, { useEffect } from 'react';
-import landscape1 from "./assets/pine-watt-unsplash.jpg"
+import React, { useEffect, useState } from 'react';
 
 import "./assets/Tabs.css"
 import selectTabFunction from "./assets/utils"
 
 function App() {
 
+  var tabData = [
+    {
+      "id": 1,
+      "title": "History",
+      "content": "I'm baby wolf pickled schlitz try-hard normcore marfa man bun mumblecore vice pop-up XOXO lomo kombucha glossier bicycle rights. Umami kinfolk salvia jean shorts offal venmo. Knausgaard tilde try-hard, woke fixie banjo man bun. Small batch tumeric mustache tbh wayfarers 8-bit shaman chartreuse tacos. Viral direct trade hoodie ugh chambray, craft beer pork belly flannel tacos single-origin coffee art party migas plaid pop-up.",
+      "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }, 
+    {
+      "id": 2,
+      "title": "Vision",
+      "content": "Lorem Ipsum 2",
+      "image": "https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }, 
+    {
+      "id": 3,
+      "title": "Goals",
+      "content": "Lorem Ipsum 3",
+      "image": "https://images.unsplash.com/photo-1511884642898-4c92249e20b6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ];
+  
   useEffect(() => {
-    selectTabFunction()
+    tabData.map((item) => {
+      console.log("aaa", item.title)
+    })
+
+    selectTabFunction(tabData)
   })
 
   return (
@@ -21,21 +45,25 @@ function App() {
 
         <div className='tabs__content'>
             <div className='tabs__img'>
-              <img src={landscape1} width={200} height={200} alt="Landscape1"/>
+              <img id="tab-img" src={tabData[0].image} width={200} height={200} alt="Landscape1"/>
             </div>
 
             <div className='tabs__info'>
               <ul className="tabs__info__menu">
-                <li className="selected">History</li>
-                <li>Vision</li>
-                <li>Goals</li>
+                {tabData.map((item, index) => {
+                    if(index===0){
+                      return <li value={index} key={index} className='selected'>{item.title}</li>  
+                    }
+                    else{
+                      return <li value={index} key={index}>{item.title}</li>
+                    }
+                  })
+                }
               </ul>
 
               <div className="tabs__info__description">
-                <b>History</b>
-                <p>
-                  I'm baby wolf pickled schlitz try-hard normcore marfa man bun mumblecore vice pop-up XOXO lomo kombucha glossier bicycle rights. Umami kinfolk salvia jean shorts offal venmo. Knausgaard tilde try-hard, woke fixie banjo man bun. Small batch tumeric mustache tbh wayfarers 8-bit shaman chartreuse tacos. Viral direct trade hoodie ugh chambray, craft beer pork belly flannel tacos single-origin coffee art party migas plaid pop-up.
-                </p>
+                <b className='tabs__description__title'>History</b>
+                <p className='tabs__description__text'>{tabData[0].content}</p>
               </div>
 
             </div>
--- src/assets/utils.js
+++ src/assets/utils.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function selectTabFunction() {
+export default function selectTabFunction(tabData) {
     const selectTab = document.querySelectorAll(".tabs__info__menu li")
     
     selectTab.forEach(function(tab) {
@@ -12,6 +12,13 @@ export default function selectTabFunction() {
         
         // Adds the Class to the clicked element;    
         tab.classList.add("selected")
+
+        // Changes the description content
+        document.querySelector(".tabs__description__title").textContent = tabData[tab.value].title;
+        document.querySelector(".tabs__description__text").textContent = tabData[tab.value].content;
+        document.querySelector("#tab-img").src = tabData[tab.value].image;
+        
       })
     })
 }
+
